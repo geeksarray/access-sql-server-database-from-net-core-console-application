498a9f7db03379ddf840e80ab933d0e753544c02
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-06-13 11:31:47 +0530

    Update Program.cs

--- AdventureData/Program.cs
+++ AdventureData/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using AdventureData.DAL;
 
 namespace AdventureData
 {
     class Program
     {
+        private static IConfiguration _iconfiguration;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GetAppSettingsFile();
+            ShowAdventureDepartments();
+            Console.WriteLine("Press any key to stop.");
+            Console.ReadKey();
+        }
+
+        static void GetAppSettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+                                 .SetBasePath(Directory.GetCurrentDirectory())
+                                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            _iconfiguration = builder.Build();
+        }        
+        static void ShowAdventureDepartments()
+        {
+            var deptDAL = new DepartmentDAL(_iconfiguration);
+            var lstDepartment = deptDAL.GetAllDepartments();
+            lstDepartment.ForEach(item =>
+            {
+                Console.WriteLine($"DeptID: {item.DepartmentID}" +
+                    $" Name: {item.Name}" +
+                    $" Grp Name: {item.GroupName}" +
+                    $" Date: {item.ModifiedDate.ToShortDateString()}");
+            });            
         }
     }
 }
